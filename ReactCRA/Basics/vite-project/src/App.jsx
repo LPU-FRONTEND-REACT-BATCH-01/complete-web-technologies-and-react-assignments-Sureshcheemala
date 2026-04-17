b7f0f39f5404614116939f15123c33c75ad788d7
Sureshcheemala <chnvss.719@gmail.com>
2026-04-17 14:32:10 +0530

    Updated states

--- ReactCRA/Basics/vite-project/src/App.jsx
+++ ReactCRA/Basics/vite-project/src/App.jsx
@@ -50,4 +50,217 @@
 
 // export default App
 
+// import { useState } from "react"
 
+// const App = () => {
+//     const [state, setState] = useState(0)
+
+//     let normal = 0
+
+//     let handleNormal = () => {
+//         normal = 10
+//     }
+
+//     let handleState = () => {
+//         setState(5)
+//     }
+
+//     return (
+//         <div>
+//             <h1>This is state variable {state}</h1>
+//             <h1>This is normal variable {normal}</h1>
+//             <button onClick={handleNormal}>Normal</button>
+//             <button onClick={handleState}>State</button>
+//         </div>
+//     )
+// }
+
+
+// export default App
+
+// import { useState } from "react"
+
+// const App = () => {
+//     let [state, setState] = useState({
+//         count:0
+//     })
+
+//     let handleIncrement = () => {
+//         setState({
+//             count:0
+//         })
+//     }
+
+//     let handleDecrememt = () => {
+//         setState(10)
+//     }
+
+//     console.log("Executed")
+
+//     return (
+//         <div>
+//             <h1>Use increment or Decrement {state.count}</h1>
+//             <button onClick={handleIncrement}>Increment</button>
+//             <button onClick={handleDecrememt}>Decrement</button>
+//         </div>
+//     )
+// }
+
+// export default App
+
+// import { useState } from "react"
+
+// const App = () => {
+//     let [state, setState] = useState({
+//         count:0
+//     })
+
+//     let handleIncrement = () => {
+//         setState({
+//             count: state.count + 1
+//         })
+//     }
+
+//     let handleDecrememt = () => {
+//         setState({
+//             count: state.count - 1
+//         })
+//     }
+
+//     console.log("Executed")
+
+//     return (
+//         <div>
+//             <h1>Use increment or Decrement {state.count}</h1>
+//             <button onClick={handleIncrement}>Increment</button>
+//             <button onClick={handleDecrememt}>Decrement</button>
+//         </div>
+//     )
+// }
+
+// export default App
+
+// import { useState } from "react"
+
+// const App = () => {
+//     let [state, setState] = useState(0)
+
+//     let handleIncrement = () => {
+//         setState(prev => prev + 1)
+//     }
+
+//     let handleDecrememt = () => {
+//         setState(prev => prev - 1)
+//     }
+
+//     console.log("Executed")
+
+//     return (
+//         <div>
+//             <h1>Use increment or Decrement {state}</h1>
+//             <button onClick={handleIncrement}>Increment</button>
+//             <button onClick={handleDecrememt}>Decrement</button>
+//         </div>
+//     )
+// }
+
+// export default App
+
+// import { useState } from "react"
+
+// const App = () => {
+//     let [show, setShow] = useState("password")
+//     let [state, setState] = useState(true)
+
+//     let handleShow = () => {
+//         if(state === true){
+//             setShow("text")
+//             setState(prev => !prev)
+//         }else{
+//             setShow("password")
+//             setState(prev => !prev)
+//         }
+//     }
+
+//     console.log("Executed")
+//     return (
+//         <div>
+//             <input type={show} />
+//             <button onClick={handleShow}>Show/Hide</button>
+//         </div>
+//     )
+// }
+
+// export default App
+
+// import { useState } from "react"
+
+// const App = () => {
+//     let [show, setShow] = useState(true)
+
+//     let handleShow = () => {
+//         setShow(prev => !prev)
+//     }
+
+//     let handleHide = () => {
+//         setShow(prev => !prev)
+//     }
+
+//     console.log("Executed")
+//     return (
+//         <div>
+//             <input type={show ? "password" : "text" } />
+//             <button onClick={show ? handleShow : handleHide}>{show ? "Show" : "Hide"}</button>
+//         </div>
+//     )
+// }
+
+// export default App
+
+// import { useState } from "react"
+
+// const App = () => {
+//     let [show, setShow] = useState(true)
+
+//     let handleShow = () => {
+//         setShow(prev => !prev)
+//     }
+
+//     let handleHide = () => {
+//         setShow(prev => !prev)
+//     }
+
+//     console.log("Executed")
+//     return (
+//         <div>
+//             <input type={show ? "password" : "text" } />
+//             <button onClick={show ? handleShow : handleHide}>
+//                 {show ? <i class="fa-solid fa-eye"></i> : <i class="fa-solid fa-eye-slash"></i>}
+//             </button>
+//         </div>
+//     )
+// }
+
+// export default App
+
+import { useState } from "react"
+import bulbOff from "./Assests/light-bulb-off.jpg"
+import bulbOn from "./Assests/light-bulb-on.avif"
+
+const App = () => {
+    let [isOn, setBulb] = useState(false)
+
+    let toggle = () => {
+        setBulb(prev => !prev)
+    }
+
+    console.log("Executed")
+    return (
+        <div>
+            <img src= {isOn ? bulbOn : bulbOff} alt="bulb" width="200" />
+            <button onClick={toggle}>{isOn ? "Turn off" : "Turn on"}</button>
+        </div>
+    )
+}
+
+export default App
